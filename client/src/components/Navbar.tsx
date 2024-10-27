5e69f4d31870a1e0ffbf104a411b4d6f0a9b6de6
HarveyBong <felix.nagy@chasacademy.se>
2024-10-27 16:53:01 +0100

    Hamburger kind of done?

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -1,4 +1,4 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { Link } from "react-router-dom";
 import { UserContext } from "../../context/userContext";
 import { useLogout } from "../hooks/useLogout";
@@ -6,39 +6,110 @@ import { useLogout } from "../hooks/useLogout";
 export default function Navbar() {
   const userContext = useContext(UserContext);
   const user = userContext?.user;
-
   const { logout } = useLogout();
-  
+  const [menuOpen, setMenuOpen] = useState(false);
+
   return (
-    <nav className="flex items-center justify-between py-8 px-10 bg-slate-100 drop-shadow-md">
-      <div className="flex items-center space-x-6">
+    <div className="w-screen py-6 px-5 lg:px-64 bg-slate-100 flex justify-between text-neutral-800 drop-shadow-md">
+      <span className="text-lg font-semibold">
         <Link to="/">Home</Link>
-        <Link to="/publish">Publicera vara</Link>
-        
-        
-        {user?.role === 'Admin' && (
-          <Link to="/admin/adminpage" className="text-blue-600 font-bold">{user.name}</Link>
-        )}
-
-        {!user && <Link to="/register">Register</Link>}
-      </div>
+      </span>
       
-      {user ? (
-        <div className="flex items-center space-x-4">
-         
-          {(user.role === 'Customer' || user.role === 'Expert') ? (
-            <Link to="/dashboard">
-              <span className="cursor-pointer font-bold">{user.name}</span>
-            </Link>
-          ) : (
-            <span className="font-bold"></span>
-          )}
+      <ul className="hidden md:flex items-center space-x-5">
+        
+        <li>
+          <Link to="/publish">Publicera vara</Link>
+        </li>
+        {user?.role === 'Admin' && (
+          <li>
+            <Link to="/admin/adminpage" className="text-blue-600 font-bold">Admin Panel</Link>
+          </li>
+        )}
+        {!user && (
+          <li>
+            <Link to="/register">Register</Link>
+          </li>
+        )}
+        {!user && (
+          <li>
+            <Link to="/login">Login</Link>
+          </li>
+        )}
+        {user && (
+          <li>
+            {(user.role === 'Customer' || user.role === 'Expert') ? (
+              <Link to="/dashboard">
+                <span className="cursor-pointer font-bold">{user.name}</span>
+              </Link>
+            ) : (
+              <span className="font-bold"></span>
+            )}
+          </li>
+        )}
+        {user && (
+          <li>
+            <button onClick={logout} className="ml-4">Logout</button>
+          </li>
+        )}
+      </ul>
 
-          <button onClick={logout} className="ml-4">Logout</button>
-        </div>
-      ) : (
-        <Link to="/login">Login</Link>
-      )}
-    </nav>
+      {/* hamburger menu */}
+      <button 
+        className="space-y-1 group md:hidden" 
+        onClick={() => setMenuOpen(!menuOpen)}
+      >
+        <div className="w-6 h-1 bg-black"></div>
+        <div className="w-6 h-1 bg-black"></div>
+        <div className="w-6 h-1 bg-black"></div>
+
+        {/* menu */}
+        {menuOpen && (
+          <ul className="bg-slate-100 w-screen pb-10 absolute top-0 right-0 duration-150 flex flex-col space-y-3 justify-end">
+            <button 
+              className="px-10 py-8 relative ml-auto"
+              onClick={() => setMenuOpen(false)}
+            >
+              <div className="w-6 h-1 rotate-45 absolute bg-black"></div>
+              <div className="w-6 h-1 -rotate-45 absolute bg-black"></div>
+            </button>
+            
+            <li className="flex justify-center w-full py-4 hover:bg-gray-200">
+              <Link to="/publish" onClick={() => setMenuOpen(false)}>Publicera vara</Link>
+            </li>
+            {user?.role === 'Admin' && (
+              <li className="flex justify-center w-full py-4 hover:bg-gray-200">
+                <Link to="/admin/adminpage" className="text-blue-600 font-bold" onClick={() => setMenuOpen(false)}>Admin Panel</Link>
+              </li>
+            )}
+            {!user && (
+              <li className="flex justify-center w-full py-4 hover:bg-gray-200">
+                <Link to="/register" onClick={() => setMenuOpen(false)}>Register</Link>
+              </li>
+            )}
+            {!user && (
+              <li className="flex justify-center w-full py-4 hover:bg-gray-200">
+                <Link to="/login" onClick={() => setMenuOpen(false)}>Login</Link>
+              </li>
+            )}
+            {user && (
+              <li className="flex justify-center w-full py-4 hover:bg-gray-200">
+                {(user.role === 'Customer' || user.role === 'Expert') ? (
+                  <Link to="/dashboard" onClick={() => setMenuOpen(false)}>
+                    <span className="cursor-pointer font-bold">{user.name}</span>
+                  </Link>
+                ) : (
+                  <span className="font-bold"></span>
+                )}
+              </li>
+            )}
+            {user && (
+              <li className="flex justify-center w-full py-4 hover:bg-gray-200">
+                <button onClick={() => { logout(); setMenuOpen(false); }}>Logout</button>
+              </li>
+            )}
+          </ul>
+        )}
+      </button>
+    </div>
   );
 }
