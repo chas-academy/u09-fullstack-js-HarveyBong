d8071ec92cf67e96a47807be63d71d2dad172ac3
HarveyBong <felix.nagy@chasacademy.se>
2024-11-23 12:31:39 +0100

    test

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -21,6 +21,7 @@ import Footer from './components/Footer';
 
 axios.defaults.baseURL = 'https://u09-fullstack-js-harveybong.onrender.com';
 axios.defaults.withCredentials = true;
+axios.defaults.headers.common['Authorization'] = `Bearer ${localStorage.getItem('token')}`;
 function RequireAdmin({ children }: { children: JSX.Element }) {
   const userContext = useContext(UserContext); // Make sure to use useContext here
 
